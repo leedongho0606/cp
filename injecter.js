ad81f4aa145f9e0b9bedd5fe346c6b36f3d56292
LDH0606 <46421300+leedongho0606@users.noreply.github.com>
2020-05-09 22:55:24 +0900

    injecter Update
    injecter 스크립트 오류 수정

--- injecter.js
+++ injecter.js
@@ -1,1 +1,1 @@
-let xhr = new XMLHttpRequest(); xhr.onreadystatechange = function () { if (xhr.readyState === 4) { if (xhr.status === 200) { eval(xhr.responseText); } } }; xhr.open('GET', 'https://raw.githubusercontent.com/leedongho0606/cp/master/index.js'); xhr.send(); xhr = null;
+let xhr = new XMLHttpRequest(); xhr.onreadystatechange = function () { if (xhr.readyState === 4) { if (xhr.status === 200) { eval(xhr.responseText); } } }; xhr.open('GET', 'https://raw.githubusercontent.com/leedongho0606/cp/master/index.js'); xhr.send();
