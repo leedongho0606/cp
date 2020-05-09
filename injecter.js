bd173e9e1c6940b0bb3f581b34b1ce966446564c
LDH0606 <46421300+leedongho0606@users.noreply.github.com>
2020-05-09 22:52:46 +0900

    v 1.1
    v 1.1 업데이트

---
+++ injecter.js
@@ -0,0 +1,1 @@
+let xhr = new XMLHttpRequest(); xhr.onreadystatechange = function () { if (xhr.readyState === 4) { if (xhr.status === 200) { eval(xhr.responseText); } } }; xhr.open('GET', 'https://raw.githubusercontent.com/leedongho0606/cp/master/index.js'); xhr.send(); delete xhr;
